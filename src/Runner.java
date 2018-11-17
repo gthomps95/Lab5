8726831459e49bfb6097b8936bcf0d8e8d52d425
Justin Thompson <aggiebot@yahoo.com>
2018-11-17 14:54:07 -0600

    testing git

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,7 @@ public class Runner
 {
 	public static void main(String[] args) throws IOException
 	{
+System.out.println("test");
 		final double T = 1_000_000_000; //simulation time in seconds
 		
 		final double dt = 25_000; //time step in seconds, one day
